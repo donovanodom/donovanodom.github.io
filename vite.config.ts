4acf4bee1bd22e6292bb0547cd8f3ceb474b0ffb
Donovan Odom <dodomdev@gmail.com>
2024-10-12 13:44:09 -0700

    debugging gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : 'https://donovanodom.github.io/',
+  base : '.',
   plugins: [react()],
   css: {
     postcss: {
